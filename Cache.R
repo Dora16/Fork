d2a11f72222daed22bd6461d888fa8f06b0f77af
Dora16 <43609466+Dora16@users.noreply.github.com>
2018-10-11 19:57:15 -0400

    Answer Cache

---
+++ Cache.R
@@ -0,0 +1,24 @@
+makeCacheMatrix<-function(x=matrix()){
+	s<-NULL
+	set<-function(y){
+		x<<-y
+		s<<-NULL
+	}
+	get<- function( ) x
+	setsol<- function(solve)    s<<-solve
+	getsol<-function( ) s
+	list(set=set, get=get,
+	      setsol=setsol,getsol=getsol)
+}
+
+cacheSolve<-function(x,...){
+	s<-x$getsol()
+	if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+     data<-x$get()
+     s <- solve(data, ...)
+     x$setsol(s)
+     s
+}
